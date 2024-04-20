bf913b42c7658b25e0a3d05fd824ac45029a7061
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-04-20 14:01:52 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   float x;
+   printf("enter a float number");
+   scanf("%f",&x);
+   int y;
+   y=x;
+   float z= x-y;
+   printf("the facto output is%f",z);
+   return 0;
+}
