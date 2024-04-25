eb7dd2be67bec24752e4ebb5070c6b6eb2a35423
G VARSHA <134367525+VARSHA-2727@users.noreply.github.com>
2024-04-25 11:54:16 +0530

    Create divBY3&&5.c

---
+++ divBY3&&5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    if(n/5&& n/3)
+    {
+        printf("the number is divisible by 3 and 5");
+    }
+    else{
+        printf("the number is not divisible by 3 and 5");
+    }
+    return 0;
+}
